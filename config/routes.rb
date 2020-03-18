86cf0a6664ec73e15b0a67500b7251c31dcb5f4e
alisagrdva <alisagrdva@gmail.com>
2020-03-18 16:44:46 +0300

    New commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :insta_posts
+  resources :insta_tags
+  resources :insta_accounts
+  resources :moniitors
+  resources :articles
+  resources :stories
+  resources :posts
+  resources :events
+  resources :topics
+  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
