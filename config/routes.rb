c2c2b0c11a7b2ad52f5c32c5e785fb9e7b19c7a6
alisagrdva <alisagrdva@gmail.com>
2020-06-11 18:02:20 +0300

    New commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :events
   resources :topics
   get 'welcome/index'
+  get 'welcome/landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
